2c581527ffbcb3f622810abb7c4527e1e98240c7
mars506606 <72367593+mars506606@users.noreply.github.com>
2020-10-19 12:35:27 +0800

    2020.10.19 爆炸特效 10628005 10713075

---
+++ vr/Assets/KID/Explosion.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class Explosion : MonoBehaviour
+    {
+        [Header("音效")]
+        public AudioClip sound;
+        [Header("音量"), Range(0, 10)]
+        public float volume = 1;
+        [Header("爆炸特效")]
+        public GameObject explosion;
+        [Header("丟擲物件標籤")]
+        public string tagThrowable = "炸彈";
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == tagThrowable)
+            {
+                GameObject temp = Instantiate(explosion, transform.position, transform.rotation);
+                GetComponent<Collider>().enabled = false;
+                GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+                Destroy(gameObject, 1);
+                Destroy(temp, 2);
+            }
+        }
+    }
+}
